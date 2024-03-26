2f439fc3b3845228ca9615e07b88418a251e83ef
cbatuic <cbalaman@uic.edu.ph>
2024-03-26 08:33:00 +0800

    Update mp5.asm

--- machineproblem/mp5.asm
+++ machineproblem/mp5.asm
@@ -1,1 +1,13 @@
+; Ensure that your code for converting characters to uppercase and lowercase only works for letters from a|A to z|Z. If the input is not a letter, avoid converting it.
 
+; Sample Output 1
+; !
+; !
+
+; Sample Output 2 (in this example, since the input is in lowercase, then convert to uppercase)
+; q
+; Q
+
+; Sample Output 3 (in this example, since the input is in uppercase, then convert to lowercase)
+; Q
+; q
